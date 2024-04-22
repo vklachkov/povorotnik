7562e2d3ccee1507757fb4583f24be7482f7d958
Valery Klachkov <zgdump@gmail.com>
2024-04-22 23:17:06 +0300

    client: Refactor prototype

--- client/src/main.rs
+++ client/src/main.rs
@@ -1,15 +1,22 @@
 mod rotate_screen;
 
+use std::sync::Arc;
+
 use anyhow::Context;
 use btleplug::{
-    api::{bleuuid::BleUuid, Central, CentralEvent, Manager as _, Peripheral, ScanFilter},
-    platform::Manager,
+    api::{Central, CentralEvent, Manager as _, Peripheral, ScanFilter},
+    platform::{Adapter, Manager, PeripheralId},
 };
 use futures::StreamExt;
+use log::warn;
+use rotate_screen::Orientation;
 use serde::Deserialize;
-use std::process::Command;
+
+const DATA_SERVICE_UUID: uuid::Uuid = uuid::uuid!("0000ff10-0000-1000-8000-00805f9b34fb");
+const DATA_CHR_UUID: uuid::Uuid = uuid::uuid!("0000ff11-0000-1000-8000-00805f9b34fb");
 
 #[derive(Debug, Deserialize)]
+#[allow(unused)]
 struct Acc {
     x: f32,
     y: f32,
@@ -31,7 +38,11 @@ async fn bluetooth_demo() -> anyhow::Result<()> {
     let manager = Manager::new().await?;
 
     let adapter_list = manager.adapters().await?;
-    let adapter = adapter_list.first().context("no Bluetooth adapter found")?;
+    let adapter = adapter_list
+        .into_iter()
+        .next()
+        .map(Arc::new)
+        .context("no Bluetooth adapter found")?;
 
     let mut events = adapter
         .events()
@@ -39,148 +50,135 @@ async fn bluetooth_demo() -> anyhow::Result<()> {
         .context("connecting to events stream")?;
 
     adapter
-        .start_scan(ScanFilter::default())
+        .start_scan(ScanFilter {
+            services: vec![DATA_SERVICE_UUID],
+        })
         .await
         .context("starting scan")?;
 
     while let Some(event) = events.next().await {
         match event {
             CentralEvent::DeviceDiscovered(id) => {
-                let peripheral = adapter
-                    .peripheral(&id)
-                    .await
-                    .context("accessing to peripheral info")?;
-
-                let properties = peripheral
-                    .properties()
-                    .await
-                    .context("reading peripheral properties")?;
-
-                let Some(properties) = properties else {
-                    continue;
-                };
-
-                let Some(name) = properties.local_name else {
-                    continue;
-                };
-
-                log::debug!("DeviceDiscovered: {name}");
-
-                if name != "LE Counter" && name != "GATT Counter" {
-                    continue;
-                }
-
-                peripheral
-                    .connect()
-                    .await
-                    .context("connecting to peripheral")?;
-
-                peripheral
-                    .discover_services()
-                    .await
-                    .context("discovering peripheral services")?;
-
-                log::debug!("Characteristics: {:?}", peripheral.characteristics());
-
-                let characteristic = peripheral
-                    .characteristics()
-                    .into_iter()
-                    .find(|chr| {
-                        chr.descriptors
-                            .iter()
-                            .find(|desc| {
-                                desc.characteristic_uuid.to_string()
-                                    == "0000ff11-0000-1000-8000-00805f9b34fb"
-                            })
-                            .is_some()
-                    })
-                    .context("LE Counter should have counter characteristic")?;
-
-                peripheral
-                    .subscribe(&characteristic)
-                    .await
-                    .context("subscribing to counter")?;
-
-                let mut notifications = peripheral
-                    .notifications()
-                    .await
-                    .context("subscribing to device notifications")?;
-
-                let mut orientation = rotate_screen::Orientation::D0;
-
-                while let Some(notification) = notifications.next().await {
-                    log::debug!(
-                        "Notification value: {:x?}",
-                        std::str::from_utf8(&notification.value)
-                    );
-
-                    let Ok(acc) = serde_json::from_slice::<Acc>(&notification.value) else {
-                        log::debug!("Invalid json");
-                        continue;
-                    };
-
-                    log::info!("Acceleration: {acc:?}");
-
-                    let Acc { x, y, .. } = acc;
-
-                    let previous_orientation = orientation;
-                    orientation = if (-0.6..0.6).contains(&x) && (0.6..1.1).contains(&y) {
-                        rotate_screen::Orientation::D0
-                    } else if (-1.1..-0.6).contains(&x) && (-0.6..0.6).contains(&y) {
-                        rotate_screen::Orientation::D90
-                    } else if (-0.6..0.6).contains(&x) && (-1.1..-0.6).contains(&y) {
-                        rotate_screen::Orientation::D180
-                    } else if (0.6..1.1).contains(&x) && (-0.6..0.6).contains(&y) {
-                        rotate_screen::Orientation::D270
-                    } else {
-                        previous_orientation
-                    };
-
-                    if orientation != previous_orientation {
-                        #[cfg(target_os = "windows")]
-                        dbg!(rotate_screen::rotate(
-                            rotate_screen::Platform::Windows,
-                            "1",
-                            orientation
-                        ));
-
-                        #[cfg(not(target_os = "windows"))]
-                        dbg!(rotate_screen::rotate(
-                            rotate_screen::Platform::Kde,
-                            "eDP-1",
-                            orientation
-                        ));
-                    }
-                }
+                log::debug!("DeviceDiscovered: '{id}'");
+                device_discovered(adapter.clone(), id);
             }
             CentralEvent::DeviceConnected(id) => {
-                log::debug!("DeviceConnected: {:?}", id);
+                log::debug!("DeviceConnected: '{id}'");
             }
             CentralEvent::DeviceDisconnected(id) => {
-                log::debug!("DeviceDisconnected: {:?}", id);
-            }
-            CentralEvent::ManufacturerDataAdvertisement {
-                id,
-                manufacturer_data,
-            } => {
-                // log::debug!(
-                //     "ManufacturerDataAdvertisement: {:?}, {:?}",
-                //     id,
-                //     manufacturer_data
-                // );
-            }
-            CentralEvent::ServiceDataAdvertisement { id, service_data } => {
-                // log::debug!("ServiceDataAdvertisement: {:?}, {:?}", id, service_data);
-            }
-            CentralEvent::ServicesAdvertisement { id, services } => {
-                let services: Vec<String> =
-                    services.into_iter().map(|s| s.to_short_string()).collect();
-
-                // log::debug!("ServicesAdvertisement: {:?}, {:?}", id, services);
+                log::debug!("DeviceDisconnected: '{id}'");
             }
             _ => {}
         }
     }
 
     Ok(())
+}
+
+fn device_discovered(adapter: Arc<Adapter>, id: PeripheralId) {
+    tokio::spawn(async move {
+        if let Err(err) = _device_discovered(adapter, id.clone()).await {
+            log::warn!("Peripheral '{id}' error: {err:#}");
+        }
+    });
+}
+
+async fn _device_discovered(adapter: Arc<Adapter>, id: PeripheralId) -> anyhow::Result<()> {
+    let peripheral = adapter
+        .peripheral(&id)
+        .await
+        .with_context(|| format!("accessing to peripheral '{id}' info"))?;
+
+    let properties = peripheral
+        .properties()
+        .await
+        .with_context(|| format!("reading peripheral '{id}' properties"))?
+        .with_context(|| format!("peripheral '{id}' should have properties"))?;
+
+    let printable_name = properties.local_name.unwrap_or_else(|| id.to_string());
+
+    peripheral
+        .connect()
+        .await
+        .with_context(|| format!("connecting to peripheral '{printable_name}'"))?;
+
+    peripheral
+        .discover_services()
+        .await
+        .with_context(|| format!("discovering peripheral '{printable_name}' services"))?;
+
+    let characteristic = peripheral
+        .characteristics()
+        .into_iter()
+        .find(|chr| {
+            chr.descriptors
+                .iter()
+                .any(|desc| desc.characteristic_uuid == DATA_CHR_UUID)
+        })
+        .with_context(|| {
+            format!("characteristic '{DATA_CHR_UUID}' not found in peripheral '{printable_name}'")
+        })?;
+
+    peripheral
+        .subscribe(&characteristic)
+        .await
+        .with_context(|| format!("subscribing to peripheral '{printable_name}' services"))?;
+
+    let mut notifications = peripheral.notifications().await.with_context(|| {
+        format!("subscribing to notifications from peripheral '{printable_name}'")
+    })?;
+
+    let mut orientation = rotate_screen::Orientation::D0;
+    while let Some(notification) = notifications.next().await {
+        if let Err(err) = handle_povorotnik_data(&mut orientation, notification.value) {
+            warn!("Failed to process data from povorotnik: {err:#}");
+        }
+    }
+
+    Ok(())
+}
+
+fn handle_povorotnik_data(orientation: &mut Orientation, data: Vec<u8>) -> anyhow::Result<()> {
+    let raw = String::from_utf8(data).context("converting notification value to string")?;
+
+    let Acc { x, y, .. } = serde_json::from_str(&raw)
+        .with_context(|| format!("deserializing notification value '{raw}'"))?;
+
+    let previous_orientation = *orientation;
+    let new_orientation = get_orientation_from_accelerometer(x, y).unwrap_or(previous_orientation);
+
+    // TODO: Remove screens hardcode
+    if new_orientation != previous_orientation {
+        #[cfg(target_os = "windows")]
+        dbg!(rotate_screen::rotate(
+            rotate_screen::Platform::Windows,
+            "1",
+            orientation
+        ));
+
+        #[cfg(not(target_os = "windows"))]
+        dbg!(rotate_screen::rotate(
+            rotate_screen::Platform::Kde,
+            "eDP-1",
+            new_orientation
+        ));
+    }
+
+    *orientation = new_orientation;
+
+    Ok(())
+}
+
+fn get_orientation_from_accelerometer(x: f32, y: f32) -> Option<Orientation> {
+    if (-0.6..0.6).contains(&x) && (0.6..1.1).contains(&y) {
+        Some(Orientation::D0)
+    } else if (-1.1..-0.6).contains(&x) && (-0.6..0.6).contains(&y) {
+        Some(Orientation::D90)
+    } else if (-0.6..0.6).contains(&x) && (-1.1..-0.6).contains(&y) {
+        Some(Orientation::D180)
+    } else if (0.6..1.1).contains(&x) && (-0.6..0.6).contains(&y) {
+        Some(Orientation::D270)
+    } else {
+        None
+    }
 }
